4791c7a8a9bb3f07f79b7e985b49809eeef2b0b4
sir-hector <krauskarol7@gmail.com>
2023-01-19 21:01:25 +0100

    add action button

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,8 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+
 type Props = {
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
@@ -48,7 +50,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>Become a member</ActionButton>
                 </div>
               </div>
             ) : (
