0e124cba6e63814a70800a45f2babadc5f40108b
sir-hector <krauskarol7@gmail.com>
2023-01-20 23:09:29 +0100

    add animations

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -17,13 +18,24 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="bg-grey-20 gap-16 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+       onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
         {/** MAIN HEADER */}
-        <div className="z-10 mt-32 md:basis-3/5">
+        <motion.div className="z-10 mt-32 md:basis-3/5">
           {/** HEADINGS */}
-          <div className="md:-mt-20">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
-              <div className="before:absolute before:-top-20 md:before:content-evolvetext before:-left-20 before:z-[-1] ">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext ">
                 <img src={HomePageText} alt="hom-page-text" />
               </div>
             </div>
@@ -33,37 +45,50 @@ const Home = ({ setSelectedPage }: Props) => {
               nemo nihil saepe nam natus earum dolore blanditiis dolores quos
               aliquid odio fuga.
             </p>
-          </div>
+          </motion.div>
           {/** ACTIONS */}
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join now
             </ActionButton>
-            <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-            onClick = {() => setSelectedPage(SelectedPage.ContactUs)}
-            href={`#${SelectedPage.ContactUs}`}
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
             >
-                <p>Learn More</p>
+              <p>Learn More</p>
             </AnchorLink>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
         {/** IMAGE */}
-        <div className="flex basis-3/5 justify-cente md:z-10
-        md:ml-40 md:mt-16 md:justify-items-end">
-            <img src={HomePageGraphic} alt="home-pageGraphic" />
+        <div
+          className="justify-cente flex basis-3/5 md:z-10
+        md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
-      </div>
+      </motion.div>
 
       {/** SPONSORS */}
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
-            <div className="mx-auto w-5/6">
-                <div className="flex w-3/5 items-center justify-between gap-8">
-                    <img src={SponsorRedBull} alt="redbull-sponsor" />
-                    <img src={SponsorFortune} alt="fortune-sponsor" />
-                    <img src={SponsorForbes} alt="forbes-sponsor" />
-                </div>
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorFortune} alt="fortune-sponsor" />
+              <img src={SponsorForbes} alt="forbes-sponsor" />
             </div>
+          </div>
         </div>
       )}
     </section>
