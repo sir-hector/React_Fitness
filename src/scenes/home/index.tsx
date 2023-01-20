ff6dfcc3089ceae093018a1b48c03e75355c12d6
sir-hector <krauskarol7@gmail.com>
2023-01-20 22:59:06 +0100

    add styling to home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -17,17 +17,17 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="bg-grey-20 gap-16 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/** MAIN HEADER */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/** HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 md:before:content-evolvetext before:-left-20 before:z-[-1] ">
                 <img src={HomePageText} alt="hom-page-text" />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
               vitae magni voluptatum, rerum explicabo, nostrum pariatur, odit
               nemo nihil saepe nam natus earum dolore blanditiis dolores quos
@@ -35,7 +35,7 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </div>
           {/** ACTIONS */}
-          <div>
+          <div className="mt-8 flex items-center gap-8">
             <ActionButton setSelectedPage={setSelectedPage}>
               Join now
             </ActionButton>
@@ -48,16 +48,17 @@ const Home = ({ setSelectedPage }: Props) => {
           </div>
         </div>
         {/** IMAGE */}
-        <div>
+        <div className="flex basis-3/5 justify-cente md:z-10
+        md:ml-40 md:mt-16 md:justify-items-end">
             <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
       </div>
 
       {/** SPONSORS */}
       {isAboveMediumScreens && (
-        <div>
-            <div>
-                <div>
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+            <div className="mx-auto w-5/6">
+                <div className="flex w-3/5 items-center justify-between gap-8">
                     <img src={SponsorRedBull} alt="redbull-sponsor" />
                     <img src={SponsorFortune} alt="fortune-sponsor" />
                     <img src={SponsorForbes} alt="forbes-sponsor" />
