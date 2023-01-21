fd1fe99b586847dace11e63aefa2ab91a8ed9799
sir-hector <krauskarol7@gmail.com>
2023-01-21 12:05:04 +0100

    create header in benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -14,11 +14,24 @@ type Props = {
 const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
-        <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
-            <div>
-                <HText>MORE THAN JUST A GYM</HText>
-            </div>
-        </motion.div>
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/** HEADER */}
+        <div className="md:my-5 md:w-3/5">
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
+            deleniti incidunt pariatur, repellat corporis commodi? Et laborum
+            voluptate aspernatur asperiores numquam dolore tenetur odio
+            obcaecati. Magnam fugiat voluptate iure laboriosam.
+          </p>
+        </div>
+        {/** BENEFITS */}
+        <div className="md:flex">
+
+        </div>
+      </motion.div>
     </section>
   );
 };
